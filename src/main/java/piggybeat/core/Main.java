87ed12aca8539b98c3220473988a594763c85e4b
BlackBlocky <125810855+BlackBlocky@users.noreply.github.com>
2024-11-11 12:43:31 +0100

    JavaFX-23 Setup done

--- src/main/java/piggybeat/core/Main.java
+++ src/main/java/piggybeat/core/Main.java
@@ -1,7 +1,21 @@
 package piggybeat.core;
 
-public class Main {
+import javafx.application.Application;
+import javafx.stage.Stage;
+import javafx.scene.Scene;
+import javafx.scene.layout.StackPane;
+
+public class Main extends Application {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        launch(args);
+    }
+
+    public void start(Stage primaryStage) throws Exception {
+        StackPane root = new StackPane();
+        Scene mainScene = new Scene(root, 300, 200);
+
+        primaryStage.setTitle("Some Testing");
+        primaryStage.setScene(mainScene);
+        primaryStage.show();
     }
 }
